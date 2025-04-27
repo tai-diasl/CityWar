ee41b90ac82766e7f54df2d6a0438fda1cd8b88f
Taís Dias <taisayana@hotmail.com>
2025-04-27 19:37:21 +0100

    Level 2 implementation

--- code/Const.py
+++ code/Const.py
@@ -11,12 +11,19 @@ COLOR_CYAN = (0, 128, 128)
 
 # Constantes com E
 EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
     'Level1Bg2': 2,
     'Level1Bg3': 3,
     'Level1Bg4': 4,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Level2Bg5': 5,
     'Player1': 3,
     'Player1Shot': 1,
     'Player2': 3,
@@ -34,11 +41,11 @@ ENTITY_HEALTH = {
     'Level1Bg3': 999,
     'Level1Bg4': 999,
     'Level2Bg0': 999,
-    # 'Level2Bg1': 999,
-    # 'Level2Bg2': 999,
-    # 'Level2Bg3': 999,
-    # 'Level2Bg4': 999,
-    # 'Level2Bg5': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
+    'Level2Bg5': 999,
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
@@ -56,12 +63,12 @@ ENTITY_DAMAGE = {
     'Level1Bg2': 0,
     'Level1Bg3': 0,
     'Level1Bg4': 0,
-    # 'Level2Bg0': 0,
-    # 'Level2Bg1': 0,
-    # 'Level2Bg2': 0,
-    # 'Level2Bg3': 0,
-    # 'Level2Bg4': 0,
-    # 'Level2Bg5': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Level2Bg5': 0,
     'Player1': 1,
     'Player1Shot': 25,
     'Player2': 2,
@@ -78,6 +85,12 @@ ENTITY_SCORE = {
     'Level1Bg2': 0,
     'Level1Bg3': 0,
     'Level1Bg4': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Level2Bg5': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -118,6 +131,9 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,  # para o Player1 atacar
 # Constantes com S
 SPAWN_TIME = 4000
 
+# Constantes com T
+TIMEOUT_LEVEL = 20000  # 20s
+TIMEOUT_STEP = 100  # 100ms
 
 # Constantes com W
 # Tamanho da janela
